6b0b7b86283378fb7eae3acb1ca66c844342a953
lara <lara@vehicle4change.org>
2021-08-26 23:14:49 -0400

    png

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(favicon(__dirname + '/public/favicon.png'));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
