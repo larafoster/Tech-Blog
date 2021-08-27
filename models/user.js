1cb7b792637c097acb3068f9fe8cc0ec2f14b88b
lara <lara@vehicle4change.org>
2021-08-27 16:04:45 -0400

    typo

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ class User extends Model {
   // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
-  }
+  } 
 }
 
 User.init(
